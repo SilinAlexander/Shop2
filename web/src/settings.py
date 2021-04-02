78286ebefb8fda048bf30e9327194f640aea5c5e
SilinAlexander <asilin1997@mail.ru>
2021-04-02 12:05:45 +0500

    9

--- web/src/settings.py
+++ web/src/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_countries',
+    'django_treebeard',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
